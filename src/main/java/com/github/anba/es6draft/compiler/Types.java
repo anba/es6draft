efa31bc97bf5f2972c400ae68be42692d6129f9f
André Bargull <andre.bargull@gmail.com>
2013-02-17 19:10:24 +0100

    add final modifier to Types class

--- src/main/java/com/github/anba/es6draft/compiler/Types.java
+++ src/main/java/com/github/anba/es6draft/compiler/Types.java
@@ -46,65 +46,65 @@ final class Types {
     }
 
     // java.lang
-    static Type Boolean = Type.getType(Boolean.class);
-    static Type CharSequence = Type.getType(CharSequence.class);
-    static Type Double = Type.getType(Double.class);
-    static Type Integer = Type.getType(Integer.class);
-    static Type Object = Type.getType(Object.class);
-    static Type Object_ = Type.getType(Object[].class);
-    static Type String = Type.getType(String.class);
-    static Type String_ = Type.getType(String[].class);
-    static Type StringBuilder = Type.getType(StringBuilder.class);
-    static Type Throwable = Type.getType(Throwable.class);
+    static final Type Boolean = Type.getType(Boolean.class);
+    static final Type CharSequence = Type.getType(CharSequence.class);
+    static final Type Double = Type.getType(Double.class);
+    static final Type Integer = Type.getType(Integer.class);
+    static final Type Object = Type.getType(Object.class);
+    static final Type Object_ = Type.getType(Object[].class);
+    static final Type String = Type.getType(String.class);
+    static final Type String_ = Type.getType(String[].class);
+    static final Type StringBuilder = Type.getType(StringBuilder.class);
+    static final Type Throwable = Type.getType(Throwable.class);
 
     // java.lang.invoke
-    static Type MethodHandle = Type.getType(MethodHandle.class);
+    static final Type MethodHandle = Type.getType(MethodHandle.class);
 
     // java.util
-    static Type ArrayList = Type.getType(ArrayList.class);
-    static Type HashMap = Type.getType(HashMap.class);
-    static Type Iterator = Type.getType(Iterator.class);
-    static Type List = Type.getType(List.class);
-    static Type Map = Type.getType(Map.class);
+    static final Type ArrayList = Type.getType(ArrayList.class);
+    static final Type HashMap = Type.getType(HashMap.class);
+    static final Type Iterator = Type.getType(Iterator.class);
+    static final Type List = Type.getType(List.class);
+    static final Type Map = Type.getType(Map.class);
 
     // es6draft.compiler
-    static Type CompiledScript = Type.getType(CompiledScript.class);
+    static final Type CompiledScript = Type.getType(CompiledScript.class);
 
     // es6draft.runtime
-    static Type AbstractOperations = Type.getType(AbstractOperations.class);
-    static Type EnvironmentRecord = Type.getType(EnvironmentRecord.class);
-    static Type ExecutionContext = Type.getType(ExecutionContext.class);
-    static Type GlobalEnvironmentRecord = Type.getType(GlobalEnvironmentRecord.class);
-    static Type LexicalEnvironment = Type.getType(LexicalEnvironment.class);
-    static Type Realm = Type.getType(Realm.class);
-    static Type ScriptRuntime = Type.getType(ScriptRuntime.class);
+    static final Type AbstractOperations = Type.getType(AbstractOperations.class);
+    static final Type EnvironmentRecord = Type.getType(EnvironmentRecord.class);
+    static final Type ExecutionContext = Type.getType(ExecutionContext.class);
+    static final Type GlobalEnvironmentRecord = Type.getType(GlobalEnvironmentRecord.class);
+    static final Type LexicalEnvironment = Type.getType(LexicalEnvironment.class);
+    static final Type Realm = Type.getType(Realm.class);
+    static final Type ScriptRuntime = Type.getType(ScriptRuntime.class);
 
     // es6draft.runtime.objects
-    static Type Eval = Type.getType(Eval.class);
+    static final Type Eval = Type.getType(Eval.class);
 
     // es6draft.runtime.types
-    static Type Callable = Type.getType(Callable.class);
-    static Type Function = Type.getType(Function.class);
-    static Type Generator = Type.getType(Generator.class);
-    static Type Null = Type.getType(Null.class);
-    static Type Reference = Type.getType(Reference.class);
-    static Type Scriptable = Type.getType(Scriptable.class);
-    static Type Scriptable_ = Type.getType(Scriptable[].class);
-    static Type _Type = Type.getType(com.github.anba.es6draft.runtime.types.Type.class);
-    static Type Undefined = Type.getType(Undefined.class);
+    static final Type Callable = Type.getType(Callable.class);
+    static final Type Function = Type.getType(Function.class);
+    static final Type Generator = Type.getType(Generator.class);
+    static final Type Null = Type.getType(Null.class);
+    static final Type Reference = Type.getType(Reference.class);
+    static final Type Scriptable = Type.getType(Scriptable.class);
+    static final Type Scriptable_ = Type.getType(Scriptable[].class);
+    static final Type _Type = Type.getType(com.github.anba.es6draft.runtime.types.Type.class);
+    static final Type Undefined = Type.getType(Undefined.class);
 
     // es6draft.runtime.types.builtins
-    static Type ExoticArguments = Type.getType(ExoticArguments.class);
-    static Type ExoticArray = Type.getType(ExoticArray.class);
-    static Type OrdinaryGenerator = Type.getType(OrdinaryGenerator.class);
-    static Type OrdinaryFunction = Type.getType(OrdinaryFunction.class);
-    static Type OrdinaryObject = Type.getType(OrdinaryObject.class);
+    static final Type ExoticArguments = Type.getType(ExoticArguments.class);
+    static final Type ExoticArray = Type.getType(ExoticArray.class);
+    static final Type OrdinaryGenerator = Type.getType(OrdinaryGenerator.class);
+    static final Type OrdinaryFunction = Type.getType(OrdinaryFunction.class);
+    static final Type OrdinaryObject = Type.getType(OrdinaryObject.class);
 
     // es6draft.runtime.internal
-    static Type RuntimeInfo = Type.getType(RuntimeInfo.class);
-    static Type RuntimeInfo$Code = Type.getType(RuntimeInfo.Code.class);
-    static Type RuntimeInfo$Function = Type.getType(RuntimeInfo.Function.class);
-    static Type RuntimeInfo$ScriptBody = Type.getType(RuntimeInfo.ScriptBody.class);
-    static Type ScriptException = Type.getType(ScriptException.class);
+    static final Type RuntimeInfo = Type.getType(RuntimeInfo.class);
+    static final Type RuntimeInfo$Code = Type.getType(RuntimeInfo.Code.class);
+    static final Type RuntimeInfo$Function = Type.getType(RuntimeInfo.Function.class);
+    static final Type RuntimeInfo$ScriptBody = Type.getType(RuntimeInfo.ScriptBody.class);
+    static final Type ScriptException = Type.getType(ScriptException.class);
 
 }
