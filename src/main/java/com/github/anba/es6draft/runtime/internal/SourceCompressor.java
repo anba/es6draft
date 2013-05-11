571157c9c42fc808cfd5da98c6cecfe03399b3c4
André Bargull <andre.bargull@gmail.com>
2013-05-11 11:04:43 +0200

    Fix that source encoding issue

--- src/main/java/com/github/anba/es6draft/runtime/internal/SourceCompressor.java
+++ src/main/java/com/github/anba/es6draft/runtime/internal/SourceCompressor.java
@@ -9,6 +9,7 @@ package com.github.anba.es6draft.runtime.internal;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.Callable;
 import java.util.zip.GZIPInputStream;
@@ -19,9 +20,18 @@ import java.util.zip.GZIPOutputStream;
  */
 public final class SourceCompressor {
     private static final int BUFFER_SIZE = 256;
-    private static final String ENCODING = StandardCharsets.ISO_8859_1.name();
 
     private SourceCompressor() {
+    }
+
+    private static class ByteOutputStream extends ByteArrayOutputStream {
+        public ByteOutputStream(int size) {
+            super(size);
+        }
+
+        public String toString(Charset cs) {
+            return new String(buf, 0, count, cs);
+        }
     }
 
     private static class CompressedSource implements Callable<String> {
@@ -33,12 +43,12 @@ public final class SourceCompressor {
 
         @Override
         public String call() throws IOException {
-            ByteArrayOutputStream bout = new ByteArrayOutputStream(BUFFER_SIZE);
+            ByteOutputStream bout = new ByteOutputStream(BUFFER_SIZE);
             GZIPOutputStream out = new GZIPOutputStream(bout, BUFFER_SIZE);
-            out.write(source.getBytes(ENCODING));
+            out.write(source.getBytes(StandardCharsets.UTF_8));
             out.close();
 
-            return bout.toString(ENCODING);
+            return bout.toString(StandardCharsets.ISO_8859_1);
         }
     }
 
@@ -51,18 +61,19 @@ public final class SourceCompressor {
 
         @Override
         public String call() throws IOException {
-            byte[] compressed = source.getBytes(ENCODING);
+            byte[] compressed = source.getBytes(StandardCharsets.ISO_8859_1);
             ByteArrayInputStream bin = new ByteArrayInputStream(compressed);
             GZIPInputStream in = new GZIPInputStream(bin, BUFFER_SIZE);
 
-            ByteArrayOutputStream bout = new ByteArrayOutputStream(BUFFER_SIZE);
+            ByteOutputStream bout = new ByteOutputStream(BUFFER_SIZE);
             byte[] buf = new byte[BUFFER_SIZE];
             int n;
             while ((n = in.read(buf)) != -1) {
                 bout.write(buf, 0, n);
             }
+            bout.close();
 
-            return new String(bout.toByteArray(), ENCODING);
+            return bout.toString(StandardCharsets.UTF_8);
         }
     }
 
