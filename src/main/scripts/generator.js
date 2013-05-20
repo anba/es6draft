0ad48b717bce4866078ea6c2db06a7fb36668c75
André Bargull <andre.bargull@gmail.com>
2013-05-20 21:11:47 +0200

    update generators.js compatibility script

--- src/main/scripts/generator.js
+++ src/main/scripts/generator.js
@@ -8,9 +8,37 @@
 (function Generator(global) {
 "use strict";
 
-const GeneratorPrototype = Object.getPrototypeOf(function*(){}).prototype;
+const Object = global.Object,
+      Function = global.Function;
 
-// remove close() function from prototype
-delete GeneratorPrototype.close;
+const Object_defineProperty = Object.defineProperty,
+      Object_hasOwnProperty = Function.prototype.call.bind(Object.prototype.hasOwnProperty);
+
+const GeneratorPrototype = Object.getPrototypeOf(function*(){}).prototype;
+const GeneratorPrototype_next = GeneratorPrototype.next;
+const genState = newSym("genState");
+
+Object.defineProperty(GeneratorPrototype, "next", {
+  value() {
+    if (!Object_hasOwnProperty(this, genState)) {
+      Object_defineProperty(this, genState, {value: true});
+    }
+    return GeneratorPrototype_next.apply(this, arguments);
+  },
+  writable: true, enumerable: false, configurable: true
+});
+
+// add send() function to prototype
+Object.defineProperty(GeneratorPrototype, "send", {
+  value() {
+    if (!Object_hasOwnProperty(this, genState)) {
+      Object_defineProperty(this, genState, {value: true});
+      // drop arguments on first call to next()
+      return GeneratorPrototype_next.call(this);
+    }
+    return GeneratorPrototype_next.apply(this, arguments);
+  },
+  writable: true, enumerable: false, configurable: true
+});
 
 })(this);
