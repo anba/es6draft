5a543ffbea95cb1a75cfe02596b2b823494b5515
André Bargull <andre.bargull@gmail.com>
2013-06-17 11:26:57 +0200

    Add support for Error.stackTraceLimit

--- src/main/scripts/stacktrace.js
+++ src/main/scripts/stacktrace.js
@@ -9,28 +9,38 @@
 "use strict";
 
 const Object = global.Object,
+      Math = global.Math,
       Error = global.Error;
 
-const Object_defineOwnProperty = Object.defineOwnProperty,
+const Object_defineProperty = Object.defineProperty,
       Error_prototype_toString = Error.prototype.toString;
+
+// stackTraceLimit defaults to 10
+Error.stackTraceLimit = 10;
 
 const getStackTrace = Object.getOwnPropertyDescriptor(Error.prototype, "stacktrace").get;
 
 delete Error.prototype.stack;
 Object.defineProperty(Error.prototype, "stack", {
   get() {
-    var stacktrace = getStackTrace.call(this);
-    if (stacktrace) {
-      var out = Error_prototype_toString.call(this);
-      for (var i = 0, len = stacktrace.length; i < len; ++i) {
-        var elem = stacktrace[i];
-        out += `\n    at ${elem.methodName} (${elem.fileName}:${elem.lineNumber})`;
-      }
-      return out;
+    var limit = Error.stackTraceLimit;
+    if (typeof limit != 'number') {
+      return;
     }
+    var stacktrace = getStackTrace.call(this);
+    if (!stacktrace) {
+      return;
+    }
+    var out = Error_prototype_toString.call(this);
+    var len = Math.min(stacktrace.length, Math.floor(limit));
+    for (var i = 0; i < len; ++i) {
+      var elem = stacktrace[i];
+      out += `\n    at ${elem.methodName} (${elem.fileName}:${elem.lineNumber})`;
+    }
+    return out;
   },
   set(v) {
-    Object_defineOwnProperty(this, "stack", {
+    Object_defineProperty(this, "stack", {
       value: v, writable: true, enumerable: true, configurable: true
     });
   },
