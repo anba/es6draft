40f21dd65526e7a990135d49d868d6ebf7f63d44
André Bargull <andre.bargull@gmail.com>
2013-06-25 14:38:20 +0200

    Add stack overflow detection to generator threads

--- src/main/java/com/github/anba/es6draft/runtime/internal/GeneratorThread.java
+++ src/main/java/com/github/anba/es6draft/runtime/internal/GeneratorThread.java
@@ -13,11 +13,14 @@ import java.util.concurrent.atomic.AtomicInteger;
  * {@link Thread} subclass used for Generators and Generator Comprehensions
  */
 public class GeneratorThread extends Thread {
+    private static final int MAX_STACK_DEPTH = 1000;
     private final Thread parent;
+    private final int stackDepth;
 
-    private GeneratorThread(Thread parent, Runnable target, String name) {
+    private GeneratorThread(Thread parent, int stackDepth, Runnable target, String name) {
         super(target, name);
         this.parent = parent;
+        this.stackDepth = stackDepth;
     }
 
     /**
@@ -41,8 +44,9 @@ public class GeneratorThread extends Thread {
         public Thread newThread(Runnable r) {
             // save caller thread to be able to restore stacktraces
             Thread parent = Thread.currentThread();
+            int depth = stackDepthOrThrow(parent);
             String name = "generator-thread-" + threads.incrementAndGet();
-            Thread newThread = new GeneratorThread(parent, r, name);
+            Thread newThread = new GeneratorThread(parent, depth, r, name);
             if (newThread.isDaemon()) {
                 newThread.setDaemon(false);
             }
@@ -50,6 +54,18 @@ public class GeneratorThread extends Thread {
                 newThread.setPriority(Thread.NORM_PRIORITY);
             }
             return newThread;
+        }
+
+        static int stackDepthOrThrow(Thread parent) {
+            if (parent instanceof GeneratorThread) {
+                int parentDepth = ((GeneratorThread) parent).stackDepth;
+                if (parentDepth >= MAX_STACK_DEPTH) {
+                    throw new StackOverflowError();
+                }
+                return parentDepth + 1;
+            } else {
+                return 1;
+            }
         }
     }
 }
