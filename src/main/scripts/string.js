a0dcbe300d7eae567eb25b95e67c21cc42b11457
André Bargull <andre.bargull@gmail.com>
2013-07-24 16:31:24 +0200

    Protect code against user-level changes

--- src/main/scripts/string.js
+++ src/main/scripts/string.js
@@ -9,15 +9,21 @@
 "use strict";
 
 const Object = global.Object,
+      Function = global.Function,
       String = global.String,
       RegExp = global.RegExp;
 
-const String_prototype_match = String.prototype.match,
+const RegExp_prototype_replace = RegExp.prototype.replace,
+      String_prototype_match = String.prototype.match,
       String_prototype_search = String.prototype.search,
       String_prototype_replace = String.prototype.replace;
 
+const $CallFunction = Function.prototype.call.bind(Function.prototype.call);
+
+const specialCharsRE = /[|^$\\()[\]{}.?*+]/g;
+
 function ToFlatPattern(p) {
-  return String_prototype_replace.call(p, /[|^$\\()[\]{}.?*+]/g, "\\$&");
+  return $CallFunction(RegExp_prototype_replace, specialCharsRE, p, "\\$&");
 }
 
 Object.defineProperties(Object.assign(String.prototype, {
@@ -25,25 +31,24 @@ Object.defineProperties(Object.assign(String.prototype, {
     if (typeof regexp == 'string' && flags !== void 0) {
       regexp = new RegExp(regexp, flags);
     }
-    return String_prototype_match.call(this, regexp);
+    return $CallFunction(String_prototype_match, this, regexp);
   },
   search(regexp, flags) {
     if (typeof regexp == 'string' && flags !== void 0) {
       regexp = new RegExp(regexp, flags);
     }
-    return String_prototype_search.call(this, regexp);
+    return $CallFunction(String_prototype_search, this, regexp);
   },
   replace(searchValue, replaceValue, flags) {
     if (typeof searchValue == 'string' && flags !== void 0) {
       searchValue = new RegExp(ToFlatPattern(searchValue), flags);
     }
-    return String_prototype_replace.call(this, searchValue, replaceValue);
+    return $CallFunction(String_prototype_replace, this, searchValue, replaceValue);
   },
 }), {
   match: {enumerable: false},
   search: {enumerable: false},
   replace: {enumerable: false},
 });
-
 
 })(this);
