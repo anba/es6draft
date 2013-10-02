f7d6fb4ff59c49bf6034bf4e642cdd3b037b06ad
André Bargull <andre.bargull@gmail.com>
2013-10-02 10:58:23 +0200

    Add missing whitespace in license header

--- src/main/java/com/github/anba/es6draft/runtime/types/Symbol.java
+++ src/main/java/com/github/anba/es6draft/runtime/types/Symbol.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2012-2013 André Bargull
- * Alle Rechte vorbehalten / All Rights Reserved. Use is subject to license terms.
+ * Alle Rechte vorbehalten / All Rights Reserved.  Use is subject to license terms.
  *
  * <https://github.com/anba/es6draft>
  */
