5400161ca4d5eed7eeebfa7adb58309b5d5614fc
André Bargull <andre.bargull@gmail.com>
2013-11-07 10:21:03 +0100

    Add tests for identifier parsing

---
+++ src/test/scripts/suite/syntax/identifier/labels.js
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2012-2013 André Bargull
+ * Alle Rechte vorbehalten / All Rights Reserved.  Use is subject to license terms.
+ *
+ * <https://github.com/anba/es6draft>
+ */
+
+const {
+  assertSyntaxError
+} = Assert;
+
+// 11.6.2.1 Keywords
+const Keywords =
+`
+break     do        in          typeof
+case      else      instanceof  var
+catch     export    new         void
+class     extends   return      while
+const     finally   super       with
+continue  for       switch      yield
+debugger  function  this
+default   if        throw
+delete    import    try
+`.trim().split(/\s+/);
+
+const KeywordsWithoutYield = [for (w of Keywords) if (w !== "yield") w];
+
+// 11.6.2.2 Future Reserved Words
+const FutureReservedWords =
+`
+enum
+`.trim().split(/\s+/);
+
+// 11.6.2.2 Future Reserved Words (Strict Mode)
+const FutureReservedWordsStrict =
+`
+implements  let      private    public
+interface   package  protected  static
+`.trim().split(/\s+/);
+
+
+// non-strict function code
+{
+  // Keywords, except for 'yield', and Future Reserved Words are not allowed as labels
+  for (let w of [...KeywordsWithoutYield, ...FutureReservedWords]) {
+    assertSyntaxError(`function f() { ${w}: ; }`);
+    assertSyntaxError(`function f() { L1: ${w}: ; }`);
+  }
+
+  // 'yield' and Future Reserved Words (Strict Mode) are allowed as labels
+  for (let w of ["yield", ...FutureReservedWordsStrict]) {
+    Function(`function f() { ${w}: ; }`);
+    Function(`function f() { L1: ${w}: ; }`);
+  }
+}
+
+// strict function code
+{
+  // Keywords, Future Reserved Words and Future Reserved Words (Strict Mode) are not allowed as labels
+  for (let w of [...Keywords, ...FutureReservedWords, ...FutureReservedWordsStrict]) {
+    assertSyntaxError(`function f() {"use strict"; ${w}: ; }`);
+    assertSyntaxError(`function f() {"use strict"; L1: ${w}: ; }`);
+  }
+}
+
+// non-strict generator code
+{
+  // Keywords, including 'yield', and Future Reserved Words are not allowed as labels
+  for (let w of [...Keywords, ...FutureReservedWords]) {
+    assertSyntaxError(`function* f() { ${w}: ; }`);
+    assertSyntaxError(`function* f() { L1: ${w}: ; }`);
+  }
+
+  // Future Reserved Words (Strict Mode) are allowed as labels
+  for (let w of FutureReservedWordsStrict) {
+    Function(`function* f() { ${w}: ; }`);
+    Function(`function* f() { L1: ${w}: ; }`);
+  }
+}
+
+// strict generator code
+{
+  // Keywords, Future Reserved Words and Future Reserved Words (Strict Mode) are not allowed as labels
+  for (let w of [...Keywords, ...FutureReservedWords, ...FutureReservedWordsStrict]) {
+    assertSyntaxError(`function* f() {"use strict"; ${w}: ; }`);
+    assertSyntaxError(`function* f() {"use strict"; L1: ${w}: ; }`);
+  }
+}
---
+++ src/test/scripts/suite/syntax/identifier/let_identifier.js
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2012-2013 André Bargull
+ * Alle Rechte vorbehalten / All Rights Reserved.  Use is subject to license terms.
+ *
+ * <https://github.com/anba/es6draft>
+ */
+
+const {
+  assertSyntaxError
+} = Assert;
+
+const bindings = {
+  letIsBoundName: [
+    "let", "[let]", "{let}", "{let: let}", "{not_let: let}", "[{let}]", "[{not_let: let}]",  "{not_let: [let]}",
+  ],
+  letIsntBoundName: [
+    "{let: not_let}", "[a = let()]",
+  ]
+};
+
+{
+  // 12.1.4.2.1  Static Semantics: Early Errors
+  // It is a Syntax Error if the BoundNames of ForBinding contains "let".
+  for (let binding of bindings.letIsBoundName) {
+    assertSyntaxError(`[for (${binding} of []) 0];`);
+  }
+  for (let binding of bindings.letIsntBoundName) {
+    Function(`[for (${binding} of []) 0];`);
+  }
+}
+
+{
+  // 13.2.1.1  Static Semantics: Early Errors
+  // It is a Syntax Error if the BoundNames of LexicalBinding contains "let".
+  for (let binding of bindings.letIsBoundName) {
+    assertSyntaxError(`let ${binding} = {};`);
+    assertSyntaxError(`const ${binding} = {};`);
+  }
+  for (let binding of bindings.letIsntBoundName) {
+    Function(`let ${binding} = {};`);
+    Function(`const ${binding} = {};`);
+  }
+}
+
+{
+  // "let" is always allowed as binding-identifier in VariableDeclaration
+  for (let binding of [...bindings.letIsBoundName, ...bindings.letIsntBoundName]) {
+    Function(`var ${binding} = {};`);
+  }
+}
+
+{
+  // "let" is always allowed as binding-identifier in CatchParameter
+  for (let binding of [...bindings.letIsBoundName, ...bindings.letIsntBoundName]) {
+    Function(`try {} catch(${binding}) {}`);
+  }
+}
+
+{
+  // "let" is always allowed as binding-identifier in FormalParameters
+  for (let binding of [...bindings.letIsBoundName, ...bindings.letIsntBoundName]) {
+    Function(`function f(${binding}) {}`);
+    Function(`function* f(${binding}) {}`);
+    Function(`((${binding}) => {});`);
+  }
+}
+
+{
+  // "let" is always allowed as binding-identifier in StrictFormalParameters
+  for (let binding of [...bindings.letIsBoundName, ...bindings.letIsntBoundName]) {
+    Function(`((${binding}) => {});`);
+    Function(`({x(${binding}) {}});`);
+    Function(`({*x(${binding}) {}});`);
+  }
+}
+
+{
+  // "let" is always allowed as binding-identifier in PropertySetParameterList
+  for (let binding of [...bindings.letIsBoundName, ...bindings.letIsntBoundName]) {
+    Function(`({set x(${binding}) {}});`);
+  }
+}
