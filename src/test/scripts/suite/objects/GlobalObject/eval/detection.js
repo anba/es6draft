bfce2474624ad9fdd1acfdfde737e2ea853e8183
André Bargull <andre.bargull@gmail.com>
2013-11-29 17:15:30 +0100

    Add test for direct eval detection

---
+++ src/test/scripts/suite/objects/GlobalObject/eval/detection.js
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2012-2013 André Bargull
+ * Alle Rechte vorbehalten / All Rights Reserved.  Use is subject to license terms.
+ *
+ * <https://github.com/anba/es6draft>
+ */
+
+const {
+  assertSame
+} = Assert;
+
+loadRelativeToScript("../../../lib/recorder.js");
+
+function HasBinding(name) {
+  return `get:Symbol(Symbol.unscopables);has:${name};`;
+}
+
+function GetBindingValue(name) {
+  return `has:${name};get:${name};`;
+}
+
+const global = this;
+
+{
+  let {object, record} = Recorder.createObject({});
+  with (object) {
+    eval("");
+  }
+  assertSame(HasBinding("eval"), record());
+}
+
+{
+  let {object, record} = Recorder.createObject({eval: () => {}});
+  with (object) {
+    eval("");
+  }
+  assertSame(HasBinding("eval") + GetBindingValue("eval"), record());
+}
+
+{
+  let {object, record} = Recorder.createObject({eval: global.eval});
+  with (object) {
+    eval("");
+  }
+  assertSame(HasBinding("eval") + GetBindingValue("eval"), record());
+}
