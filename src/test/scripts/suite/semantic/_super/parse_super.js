65a14d4350eab5c14a44d3846a76600d3740c357
André Bargull <andre.bargull@gmail.com>
2013-12-04 12:03:45 +0100

    Simple test case to show [[HomeObject]] does not bind to [[Prototype]]

---
+++ src/test/scripts/suite/semantic/_super/parse_super.js
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2012-2013 André Bargull
+ * Alle Rechte vorbehalten / All Rights Reserved.  Use is subject to license terms.
+ *
+ * <https://github.com/anba/es6draft>
+ */
+
+const {
+  assertSame
+} = Assert;
+
+// [[HomeObject]] is not bound to [[Prototype]]
+{
+  let log = "";
+
+  class Base {
+    p() { log += "[Base]"; }
+  }
+  class OtherBase {
+    p() { log += "[OtherBase]"; }
+  }
+  class Derived extends Base {
+    p() {
+      log += "[Derived]";
+      super();
+      Object.setPrototypeOf(Derived.prototype, OtherBase.prototype);
+      super();
+    }
+  }
+  (new Derived).p();
+
+  assertSame("[Derived][Base][OtherBase]", log);
+}
