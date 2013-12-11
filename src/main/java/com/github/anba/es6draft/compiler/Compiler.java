4557c85278751015074c4798650eb8f826de0b0b
André Bargull <andre.bargull@gmail.com>
2013-12-11 12:22:44 +0100

    Add simple debug mode

--- src/main/java/com/github/anba/es6draft/compiler/Compiler.java
+++ src/main/java/com/github/anba/es6draft/compiler/Compiler.java
@@ -16,8 +16,11 @@ import java.util.Locale;
 
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Handle;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
+import org.objectweb.asm.util.Printer;
+import org.objectweb.asm.util.Textifier;
 import org.objectweb.asm.util.TraceClassVisitor;
 
 import com.github.anba.es6draft.ast.FunctionDefinition;
@@ -52,7 +55,7 @@ public class Compiler {
     }
 
     public enum Option {
-        Debug, SourceMap
+        Debug, FullDebug, SourceMap
     }
 
     private final EnumSet<Option> compilerOptions;
@@ -157,9 +160,97 @@ public class Compiler {
         return EnumSet.noneOf(CompatibilityOption.class);
     }
 
-    private static void debug(byte[] b) {
+    private void debug(byte[] b) {
         ClassReader cr = new ClassReader(b);
-        cr.accept(new TraceClassVisitor(new PrintWriter(System.out)), 0);
+        Printer p;
+        if (compilerOptions.contains(Option.FullDebug)) {
+            p = new Textifier();
+        } else {
+            p = new SimpleTypeTextifier();
+        }
+        cr.accept(new TraceClassVisitor(null, p, null), 0);
+        PrintWriter pw = new PrintWriter(System.out);
+        p.print(pw);
+        pw.flush();
+    }
+
+    private static class SimpleTypeTextifier extends Textifier {
+        SimpleTypeTextifier() {
+            super(Opcodes.ASM4);
+        }
+
+        @Override
+        protected Textifier createTextifier() {
+            return new SimpleTypeTextifier();
+        }
+
+        private String getDescriptor(Type type) {
+            if (type.getSort() == Type.OBJECT) {
+                String name = type.getInternalName();
+                int index = name.lastIndexOf('/');
+                return name.substring(index + 1);
+            }
+            if (type.getSort() == Type.ARRAY) {
+                StringBuilder sb = new StringBuilder(getDescriptor(type.getElementType()));
+                for (int dim = type.getDimensions(); dim > 0; --dim) {
+                    sb.append("[]");
+                }
+                return sb.toString();
+            }
+            return type.getClassName();
+        }
+
+        private String getInternalName(String internalName) {
+            return getDescriptor(Type.getObjectType(internalName));
+        }
+
+        private String getDescriptor(String typeDescriptor) {
+            return getDescriptor(Type.getType(typeDescriptor));
+        }
+
+        private String getMethodDescriptor(String methodDescriptor) {
+            Type[] argumentTypes = Type.getArgumentTypes(methodDescriptor);
+            Type returnType = Type.getReturnType(methodDescriptor);
+
+            StringBuilder sb = new StringBuilder();
+            sb.append('(');
+            for (int i = 0; i < argumentTypes.length; i++) {
+                sb.append(getDescriptor(argumentTypes[i]));
+                if (i + 1 < argumentTypes.length) {
+                    sb.append(", ");
+                }
+            }
+            sb.append(')');
+            sb.append(getDescriptor(returnType));
+
+            return sb.toString();
+        }
+
+        @Override
+        public void visitLdcInsn(Object cst) {
+            if (cst instanceof Handle) {
+                Handle handle = (Handle) cst;
+                cst = new Handle(handle.getTag(), handle.getOwner(), handle.getName(),
+                        getMethodDescriptor(handle.getDesc()));
+            }
+            super.visitLdcInsn(cst);
+        }
+
+        @Override
+        protected void appendDescriptor(int type, String desc) {
+            switch (type) {
+            case INTERNAL_NAME:
+                desc = getInternalName(desc);
+                break;
+            case FIELD_DESCRIPTOR:
+                desc = getDescriptor(desc);
+                break;
+            case METHOD_DESCRIPTOR:
+                desc = getMethodDescriptor(desc);
+                break;
+            }
+            super.appendDescriptor(type, desc);
+        }
     }
 
     private String sourceMap(Script script) {
