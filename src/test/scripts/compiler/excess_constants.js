1462c925f5997fefc022c9cf24521dc01d3f7804
André Bargull <andre.bargull@gmail.com>
2014-01-13 16:51:05 +0100

    Support excess use of constants by delegating constants storage to external classes

---
+++ src/test/scripts/compiler/excess_constants.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2012-2013 André Bargull
+ * Alle Rechte vorbehalten / All Rights Reserved.  Use is subject to license terms.
+ *
+ * <https://github.com/anba/es6draft>
+ */
+
+// excess string constants
+Function(new Array(0x7fff).fill(0).map((x, i) => `f('${i}')`).join(";"))
+
+// excess number constants
+Function(new Array(0x7fff).fill(0).map((x, i) => `f(${1 / i})`).join(";"))
