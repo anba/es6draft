cc53ffaee6538faeac50822bce5468bd6d8b0275
André Bargull <andre.bargull@gmail.com>
2014-01-21 11:48:58 +0100

    Add some tests for Promise.all

--- src/test/scripts/suite/objects/Promise/all.js
+++ src/test/scripts/suite/objects/Promise/all.js
@@ -7,10 +7,154 @@
 
 const {
   assertBuiltinFunction,
+  fail,
+  assertSame, assertEquals,
+  assertFalse, assertTrue,
 } = Assert;
 
+loadRelativeToScript("../../lib/promises.js");
+
+const {
+  reportFailure
+} = Promises;
 
 /* Promise.all ( iterable ) */
 
 assertBuiltinFunction(Promise.all, "all", 1);
 
+// Empty iterables
+{
+  for (let iterable of [[], function*(){}(), {[Symbol.iterator]: () => ({next: () => ({done: true})})}]) {
+    let count = 0;
+    Promise
+      .all(iterable)
+      .then(values => {
+        assertSame(0, count++);
+        assertEquals([], values);
+      })
+      .catch(reportFailure);
+    Promise.resolve().then(() => assertSame(1, count)).catch(reportFailure);
+  }
+}
+
+// Promise.all and rejected promises
+{
+  // No .then() for rejected promises
+  Promise
+    .all([Promise.reject(0)])
+    .then(v => fail `fulfilled with ${v}`, v => { assertSame(0, v) })
+    .catch(reportFailure);
+  Promise
+    .all([Promise.reject(0), Promise.resolve(1)])
+    .then(v => fail `fulfilled with ${v}`, v => { assertSame(0, v) })
+    .catch(reportFailure);
+  Promise
+    .all([Promise.resolve(1), Promise.reject(0)])
+    .then(v => fail `fulfilled with ${v}`, v => { assertSame(0, v) })
+    .catch(reportFailure);
+}
+
+function tamper(p) {
+  return Object.assign(p, {
+    then(onFulfilled, onRejected) {
+      onFulfilled();
+      return Promise.prototype.then.call(this, onFulfilled, onRejected);
+    }
+  });
+}
+
+// Manual dispatch of Promise.all Countdown Functions (1)
+{
+  function* g() {
+    yield tamper(Promise.resolve(0));
+  }
+
+  // Prevent countdownHolder.[[Countdown]] from ever reaching zero
+  Promise
+    .all(g())
+    .then(() => { throw new Error("resolve called") })
+    .catch(reportFailure);
+}
+
+// Manual dispatch of Promise.all Countdown Functions (2)
+{
+  function* g() {
+    yield Promise.resolve(0);
+    yield tamper(Promise.resolve(1));
+    yield Promise.resolve(2)
+      .then(() => {
+        assertFalse(fulfillCalled);
+      })
+      .then(() => {
+        assertTrue(fulfillCalled);
+      })
+      .catch(reportFailure);
+  }
+
+  // Promise from Promise.all resolved before arguments
+  let fulfillCalled = false;
+  Promise
+    .all(g())
+    .then(() => {
+      assertFalse(fulfillCalled);
+      fulfillCalled = true;
+    })
+    .catch(reportFailure);
+}
+
+// Manual dispatch of Promise.all Countdown Functions (3)
+{
+  function* g() {
+    yield Promise.resolve(0);
+    yield tamper(Promise.resolve(1));
+    yield Promise.reject(2);
+  }
+
+  // Promise from Promise.all resolved despite rejected promise in arguments
+  Promise
+    .all(g())
+    .then(v => {
+      assertEquals([0, 1], v);
+    }, reportFailure)
+    .catch(reportFailure);
+}
+
+// Manual dispatch of Promise.all Countdown Functions (4)
+{
+  let hijack = true;
+  class P extends Promise {
+    constructor(resolver) {
+      if (hijack) {
+        hijack = false;
+        super((resolve, reject) => {
+          return resolver(values => {
+            actualArguments.push(values.slice());
+            return resolve(values);
+          }, reject);
+        });
+      } else {
+        super(resolver);
+      }
+    }
+
+    static cast(p) {
+      return p;
+    }
+  }
+  function* g() {
+    yield Promise.resolve(0);
+    yield tamper(Promise.resolve(1));
+    yield Promise.resolve(2);
+  }
+  let actualArguments = [];
+  let expectedArguments = [];
+  expectedArguments.push([, void 0]);
+  expectedArguments.push([0, 1]);
+
+  // Promise.all calls resolver twice
+  P.all(g()).catch(reportFailure);
+  Promise
+    .resolve()
+    .then(() => assertEquals(expectedArguments, actualArguments))
+    .catch(reportFailure);
+}
