0b48534c3edfe5cf3842359da726979363af0b46
André Bargull <andre.bargull@gmail.com>
2014-04-22 10:43:09 +0200

    Add regression test for bug 1390

---
+++ src/test/scripts/suite/regress/bug1390.js
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2012-2014 André Bargull
+ * Alle Rechte vorbehalten / All Rights Reserved.  Use is subject to license terms.
+ *
+ * <https://github.com/anba/es6draft>
+ */
+
+const {
+  assertSame
+} = Assert;
+
+// Wrong [[Scope]] for eval'ed function declarations
+// https://bugs.ecmascript.org/show_bug.cgi?id=1390
+
+var result = (function(){
+  var x = "outer";
+  try {
+    throw "inner";
+  } catch(x) {
+    return eval("function f(){ return x } f()")
+  }
+})();
+assertSame("inner", result);
+
+var result = (function(){
+  var x = "outer";
+  with({x: "inner"}) {
+    return eval("function f(){ return x } f()")
+  }
+})();
+assertSame("inner", result);
