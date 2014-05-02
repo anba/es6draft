a664397d1ff496346d7c65518c1d2535085f489c
André Bargull <andre.bargull@gmail.com>
2014-05-02 10:43:11 +0200

    Change Symbol.length to 1 (rev24)

--- src/main/java/com/github/anba/es6draft/runtime/objects/SymbolConstructor.java
+++ src/main/java/com/github/anba/es6draft/runtime/objects/SymbolConstructor.java
@@ -85,7 +85,7 @@ public final class SymbolConstructor extends BuiltinConstructor implements Initi
 
         @Value(name = "length", attributes = @Attributes(writable = false, enumerable = false,
                 configurable = true))
-        public static final int length = 0;
+        public static final int length = 1;
 
         @Value(name = "name", attributes = @Attributes(writable = false, enumerable = false,
                 configurable = true))
