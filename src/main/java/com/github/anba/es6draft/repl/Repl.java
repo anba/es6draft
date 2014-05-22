917351ec9f719aa65e4db5930824f7dcaa04df91
André Bargull <andre.bargull@gmail.com>
2014-05-22 13:21:34 +0200

    Add localization support to Repl

--- src/main/java/com/github/anba/es6draft/repl/Repl.java
+++ src/main/java/com/github/anba/es6draft/repl/Repl.java
@@ -11,6 +11,7 @@ import static com.github.anba.es6draft.runtime.AbstractOperations.CreateArrayFro
 import static com.github.anba.es6draft.runtime.types.Undefined.UNDEFINED;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
@@ -19,12 +20,14 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.EnumSet;
-import java.util.Formatter;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -63,6 +66,7 @@ import com.github.anba.es6draft.runtime.Task;
 import com.github.anba.es6draft.runtime.World;
 import com.github.anba.es6draft.runtime.internal.CompatibilityOption;
 import com.github.anba.es6draft.runtime.internal.ObjectAllocator;
+import com.github.anba.es6draft.runtime.internal.PropertiesReaderControl;
 import com.github.anba.es6draft.runtime.internal.ScriptCache;
 import com.github.anba.es6draft.runtime.internal.ScriptException;
 import com.github.anba.es6draft.runtime.internal.Strings;
@@ -73,12 +77,15 @@ import com.github.anba.es6draft.runtime.types.ScriptObject;
  * Simple REPL
  */
 public final class Repl {
+    private static final String BUNDLE_NAME = "com.github.anba.es6draft.repl.messages";
     private static final String PROGRAM_NAME = "es6draft";
+    private static final String PROMPT = "js> ";
     private static final int STACKTRACE_DEPTH = 20;
 
     public static void main(String[] args) throws Throwable {
+        Options options = new Options();
         try {
-            Options options = readOptions(args);
+            parseOptions(options, args);
             ReplConsole console = createConsole(options);
             new Repl(console, options).loop();
         } catch (Throwable e) {
@@ -111,15 +118,14 @@ public final class Repl {
         e.printStackTrace();
     }
 
-    private static Options readOptions(String[] args) {
-        Options options = new Options();
+    private static void parseOptions(Options options, String[] args) {
         CmdLineParser parser = new CmdLineParser(options);
         parser.setUsageWidth(120);
         try {
             parser.parseArgument(args);
         } catch (CmdLineException e) {
             System.err.println(e.getMessage());
-            System.err.println(getUsageString(parser));
+            System.err.println(getUsageString(parser, false));
             System.exit(1);
         }
         if (options.showVersion) {
@@ -127,7 +133,11 @@ public final class Repl {
             System.exit(0);
         }
         if (options.showHelp) {
-            System.out.println(getUsageString(parser));
+            System.out.println(getUsageString(parser, false));
+            System.exit(0);
+        }
+        if (options.showExtendedHelp) {
+            System.out.println(getUsageString(parser, true));
             System.exit(0);
         }
         if (options.debug || options.fullDebug) {
@@ -142,7 +152,6 @@ public final class Repl {
             // Default to interactive mode when no files or expressions were set
             options.interactive = true;
         }
-        return options;
     }
 
     private static interface EvalScript {
@@ -185,8 +194,8 @@ public final class Repl {
         public String getSource() throws IOException {
             Path filePath = path.toAbsolutePath();
             if (!Files.exists(filePath)) {
-                System.err.printf("File '%s' not found!%n", filePath);
-                return null;
+                String message = formatMessage("file_not_found", filePath.toString());
+                throw new FileNotFoundException(message);
             }
             byte[] content = Files.readAllBytes(filePath);
             String source = new String(content, StandardCharsets.UTF_8);
@@ -207,68 +216,69 @@ public final class Repl {
     public static final class Options {
         List<EvalScript> evalScripts = new ArrayList<>();
 
-        @Option(name = "-v", aliases = { "--version" },
-                usage = "Print version information and exit")
+        @Option(name = "-v", aliases = { "--version" }, usage = "options.version")
         boolean showVersion;
 
-        @Option(name = "-h", aliases = { "--help" }, usage = "Print this help")
+        @Option(name = "-h", aliases = { "--help" }, usage = "options.help")
         boolean showHelp;
 
-        @Option(name = "-e", aliases = { "--eval", "--execute" }, metaVar = "STRING",
-                usage = "Script code to evaluate")
+        @Option(name = "-e", aliases = { "--eval", "--execute" }, metaVar = "meta.string",
+                usage = "options.eval")
         void setEvalExpression(String expression) {
             evalScripts.add(new EvalString(expression));
         }
 
-        @Option(name = "-f", aliases = { "--file" }, metaVar = "FILE",
-                usage = "Script file to evaluate")
+        @Option(name = "-f", aliases = { "--file" }, metaVar = "meta.file", usage = "options.file")
         void setFile(Path path) {
             evalScripts.add(new EvalPath(path));
         }
 
-        @Option(name = "-i", aliases = { "--interactive" }, usage = "Start interactive mode")
+        @Option(name = "-i", aliases = { "--interactive" }, usage = "options.interactive")
         boolean interactive;
 
-        @Option(name = "--strict", usage = "Strict semantics without web compatibility")
+        @Option(name = "--strict", usage = "options.strict")
         boolean strict;
 
-        @Option(name = "--shell", usage = "Set default shell emulation (default = simple)")
+        @Option(name = "--shell", usage = "options.shell")
         ShellMode shellMode = ShellMode.Simple;
 
-        @Option(name = "--async", usage = "Enable experimental support for async functions")
+        @Option(name = "--async", usage = "options.async")
         boolean asyncFunctions;
 
-        @Option(name = "--no-jline", usage = "Disable JLine support")
+        @Option(name = "--no-jline", usage = "options.no_jline")
         boolean noJLine;
 
-        @Option(name = "--no-color", usage = "Disable colored output")
+        @Option(name = "--no-color", usage = "options.no_color")
         boolean noColor;
 
         @Option(name = "--no-interpreter", aliases = { "--compile-only" },
-                usage = "Disable interpreter")
+                usage = "options.no_interpreter")
         boolean noInterpreter;
 
-        @Option(name = "--stacktrace", usage = "Print stack-trace on error")
+        @Option(name = "--stacktrace", usage = "options.stacktrace")
         boolean stacktrace;
 
-        @Option(name = "--debug", usage = "Print generated Java bytecode")
+        @Option(name = "--stacktrace-depth", hidden = true, usage = "options.stacktrace_depth")
+        int stacktraceDepth = STACKTRACE_DEPTH;
+
+        @Option(name = "--debug", usage = "options.debug")
         boolean debug;
 
-        @Option(name = "--full-debug", hidden = true,
-                usage = "Print generated Java bytecode (full type descriptors)")
+        @Option(name = "--full-debug", hidden = true, usage = "options.full_debug")
         boolean fullDebug;
 
-        @Option(name = "--verify-stack", hidden = true,
-                usage = "Enable stack usage tracking during compilation")
+        @Option(name = "--verify-stack", hidden = true, usage = "options.verify_stack")
         boolean verifyStack;
 
-        @Argument(index = 0, multiValued = false, metaVar = "FILE",
-                usage = "Script file to execute")
+        @Option(name = "--xhelp", hidden = true, usage = "options.extended_help")
+        boolean showExtendedHelp;
+
+        @Argument(index = 0, multiValued = false, metaVar = "meta.file", usage = "options.filename")
         Path fileName = null;
 
         @Option(name = "--", handler = StopOptionAndConsumeRestHandler.class)
-        @Argument(index = 1, multiValued = true, metaVar = "ARGUMENTS", usage = "Script arguments",
-                handler = RestOfArgumentsHandler.class)
+        @Argument(index = 1, multiValued = true, metaVar = "meta.arguments",
+                usage = "options.arguments", handler = RestOfArgumentsHandler.class)
         List<String> arguments = new ArrayList<>();
     }
 
@@ -295,23 +305,69 @@ public final class Repl {
         }
     }
 
-    private static String getVersionString() {
-        return getResourceInfo("/version", PROGRAM_NAME);
+    private static String getUsageString(CmdLineParser parser, boolean showAll) {
+        ResourceBundle rb = getResourceBundle();
+        StringWriter writer = new StringWriter();
+        writer.write(formatMessage(rb, "usage", getVersionString(), PROGRAM_NAME));
+        parser.printUsage(writer, rb, showAll ? OptionHandlerFilter.ALL
+                : OptionHandlerFilter.PUBLIC);
+        return writer.toString();
     }
 
-    private static String getUsageString(CmdLineParser parser) {
-        try (Formatter formatter = new Formatter()) {
-            formatter.format("%s%n%n", getVersionString());
-            formatter.format("Usage:%n");
-            formatter.format("%s [options] [FILE [ARGUMENTS]]%n%n", PROGRAM_NAME);
-            formatter.format("Options:%n");
-            StringWriter writer = new StringWriter();
-            parser.printUsage(writer, null, OptionHandlerFilter.PUBLIC);
-            formatter.out().append(writer.toString());
-            return formatter.toString();
-        } catch (IOException e) {
-            throw new IllegalStateException(e);
+    private static ResourceBundle getResourceBundle() {
+        ResourceBundle.Control control = new PropertiesReaderControl(StandardCharsets.UTF_8);
+        return new XResourceBundle(ResourceBundle.getBundle(BUNDLE_NAME, Locale.getDefault(),
+                control));
+    }
+
+    private static final class XResourceBundle extends ResourceBundle {
+        private final ResourceBundle bundle;
+
+        XResourceBundle(ResourceBundle bundle) {
+            this.bundle = bundle;
         }
+
+        @Override
+        protected Object handleGetObject(String key) {
+            if (bundle.containsKey(key)) {
+                return bundle.getObject(key);
+            }
+            // TODO: Workarounds for https://github.com/kohsuke/args4j/issues/70
+            // and https://github.com/kohsuke/args4j/issues/71
+            return key;
+        }
+
+        @Override
+        public Enumeration<String> getKeys() {
+            return bundle.getKeys();
+        }
+
+        @Override
+        public Locale getLocale() {
+            return bundle.getLocale();
+        }
+
+        @Override
+        public boolean containsKey(String key) {
+            return bundle.containsKey(key);
+        }
+
+        @Override
+        public Set<String> keySet() {
+            return bundle.keySet();
+        }
+    }
+
+    private static String formatMessage(String key, String... messageArguments) {
+        return formatMessage(getResourceBundle(), key, messageArguments);
+    }
+
+    private static String formatMessage(ResourceBundle rb, String key, String... messageArguments) {
+        return new MessageFormat(rb.getString(key), rb.getLocale()).format(messageArguments);
+    }
+
+    private static String getVersionString() {
+        return getResourceInfo("/version", PROGRAM_NAME);
     }
 
     private static String getResourceInfo(String resourceName, String defaultValue) {
@@ -360,7 +416,8 @@ public final class Repl {
     }
 
     private void handleException(Realm realm, ScriptException e) {
-        console.printf("uncaught exception: %s%n", e.getMessage(realm.defaultContext()));
+        String message = formatMessage("uncaught_exception", e.getMessage(realm.defaultContext()));
+        console.printf("%s%n", message);
         if (options.stacktrace) {
             printStackTrace(e);
         }
@@ -419,7 +476,7 @@ public final class Repl {
      */
     private com.github.anba.es6draft.ast.Script read(Realm realm, int line) {
         StringBuilder source = new StringBuilder();
-        for (String prompt = "js> ";; prompt = "") {
+        for (String prompt = PROMPT;; prompt = "") {
             String s = console.readLine(prompt);
             if (s == null) {
                 continue;
@@ -569,8 +626,8 @@ public final class Repl {
                 parserOptions, compilerOptions);
         final ShellGlobalObject global = world.newGlobal();
         final Realm realm = global.getRealm();
-        final ScriptObject globalThis = realm.getGlobalThis();
-        final ExecutionContext cx = realm.defaultContext();
+        ScriptObject globalThis = realm.getGlobalThis();
+        ExecutionContext cx = realm.defaultContext();
 
         // Add completion to console
         console.addCompletion(realm);
@@ -605,10 +662,6 @@ public final class Repl {
                     try {
                         String sourceName = evalScript.getSourceName();
                         String source = evalScript.getSource();
-                        if (source == null) {
-                            // Return if source not available
-                            return;
-                        }
                         try {
                             eval(realm, parse(realm, sourceName, source, 1));
                         } catch (ParserException e) {
